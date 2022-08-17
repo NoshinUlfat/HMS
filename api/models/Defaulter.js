6aeface2c20c3a2993e1692ffa8d4891ad1ed60f
FahmidAR <1705087@ugrad.cse.buet.ac.bd>
2022-08-17 22:29:16 +0600

    Dining , Due , Defaulter

---
+++ api/models/Defaulter.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+ 
+const defaulterSchema = mongoose.Schema({
+    
+    studentsId: {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "Student",
+        required : true
+    },
+    dueId: {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "Due",
+        required : true
+    },
+    message: {type: String, maxLength: 200},
+    dueType : { type: String, required: true},
+    dueAmount : { type: Number, required: true},
+ 
+});
+ 
+export default mongoose.model("Defaulter", defaulterSchema);
---
+++ api/models/Due.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const dueSchema = mongoose.Schema({
+
+    dueType : { type: String, required: true},
+    dueAmount : { type: Number, required: true},
+
+});
+
+export default mongoose.model("Due", dueSchema);

21bf7c02c2c06fb8eeb0aec91fde491fa3b43aaa
FahmidAR <1705087@ugrad.cse.buet.ac.bd>
2022-08-26 15:45:13 +0600

    button color - Last after conflict solve try

---
+++ user_site/src/components/notification/Notification.jsx
@@ -0,0 +1,17 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import React from "react";
+import "./notification.scss";
+
+const Notification = () => {
+  return (
+
+    <div className="notificationModal" >
+        
+
+    </div>
+    
+  );
+};
+
+export default Notification;
